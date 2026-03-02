b88e6b60b643dfbb62c47c369851db09f4b4b4e5
nathanyathaimassage <nathanyathaimassage@gmail.com>
2026-03-02 18:21:56 +0700

    Fix next.config.js: use remotePatterns instead of deprecated domains

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [],
-    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+    ],
   },
 }
 
